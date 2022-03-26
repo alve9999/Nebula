3f5a36d18060eb58ec8adae9b20958c161ad4913
wallespion <vilhelm.lindell123@gmail.com>
2022-03-26 10:29:03 +0100

    addedcmake

---
+++ build/CMakeFiles/Nebula.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nebula.dir/main.cpp.obj"
+  "CMakeFiles/Nebula.dir/main.cpp.obj.d"
+  "Nebula.exe"
+  "Nebula.exe.manifest"
+  "Nebula.pdb"
+  "libNebula.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nebula.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
