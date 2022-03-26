a0421b53b8e3e87417711bcc85b559ea33d8b3cf
alve9999 <alve.lindell@gmail.com>
2022-03-26 19:34:07 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <iostream>
+#include "init_graphics.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}  
 
 void processInput(GLFWwindow *window)
 {
@@ -16,36 +13,7 @@ void processInput(GLFWwindow *window)
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    //init glad
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    //set viewport
-    glViewport(0, 0, 800, 600);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+    GLFWwindow* window = init_graphics(800, 600);
 
     /* Loop until the user closes the window */
     while(!glfwWindowShouldClose(window))
