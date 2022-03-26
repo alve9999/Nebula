4cbf0a97d4287b093007f001c57a96f185f4b620
alve9999 <alve.lindell@gmail.com>
2022-03-26 18:21:26 +0100

    added init glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <iostream>
 #define pi 3.1415926535897932384626433832795
 #define print(x) std::cerr << x << std::endl
-
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -22,6 +24,15 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    //init glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //set viewport
+    glViewport(0, 0, 800, 600);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
