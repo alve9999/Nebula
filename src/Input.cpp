e2c7495835e973724843e73fc9b65d4b4eb40272
wallespion <vilhelm.lindell123@gmail.com>
2022-03-27 13:26:39 +0200

    changed naming

--- src/Input.cpp
+++ src/Input.cpp
@@ -2,14 +2,13 @@
 #include <SDL2/SDL.h>
 #include "Input.h"
 
-
-glm::ivec2 Input::mouse_pos = glm::ivec2(0,0);
 const uint8_t* Input::state = SDL_GetKeyboardState(NULL);
 const uint8_t* Input::prev_state = SDL_GetKeyboardState(NULL);
+glm::ivec2 Input::mouse_pos = glm::ivec2(0,0);
 uint32_t Input::buttons = 0;
 uint32_t Input::prev_buttons = 0;
 
-void Input::update()
+void Input::Update()
 {
     prev_state = state;
     prev_buttons = buttons;
@@ -17,42 +16,42 @@ void Input::update()
     buttons = SDL_GetMouseState(&mouse_pos.x, &mouse_pos.y);
 }
 
-bool Input::key_down(int key)
+bool Input::KeyDown(int key)
 {
     return state[key];
 }
 
-bool Input::key_up(int key)
+bool Input::KeyUp(int key)
 {
     return !state[key];
 }
 
-bool Input::key_pressed(int key)
+bool Input::KeyPressed(int key)
 {
     return !prev_state[key] && state[key];
 }
 
-bool Input::key_released(int key)
+bool Input::KeyReleased(int key)
 {
     return prev_state[key] && !state[key];
 }
 
-bool Input::mouse_down(int button)
+bool Input::MouseDown(int button)
 {
     return buttons && button;
 }
 
-bool Input::mouse_up(int button)
+bool Input::MouseUp(int button)
 {
     return buttons && !button;
 }
 
-bool Input::mouse_pressed(int button)
+bool Input::MousePressed(int button)
 {
     return (prev_buttons && !button) && (prev_buttons && button);
 }
 
-bool Input::mouse_released(int button)
+bool Input::MouseReleased(int button)
 {
     return (prev_buttons && button) && (prev_buttons && !button);
 }
