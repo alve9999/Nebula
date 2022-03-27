6f356960b0dbeb9bc1ea1f8c48e3c8da9e94f202
alve9999 <alve.lindell@gmail.com>
2022-03-27 16:25:34 +0200

    fixed image loading

--- src/asset_loading.cpp
+++ src/asset_loading.cpp
@@ -6,7 +6,7 @@
 
 SDL_Texture* load_texture(const char* asset_path)
 {
-    SDL_Surface* temp_surface = IMG_Load(asset_path);
+    SDL_Surface* temp_surface = SDL_LoadBMP(asset_path);
     SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, temp_surface);
     SDL_FreeSurface(temp_surface);
     return tex;
