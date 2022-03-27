99f9d6dc6259652752713a275de46f740e2d3703
wallespion <vilhelm.lindell123@gmail.com>
2022-03-27 18:26:35 +0200

    changed init

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -3,9 +3,12 @@
 #include <SDL2/SDL.h>
 
 Sprite::Sprite(const char* asset_path, Rect src_rect = Rect(), glm::vec2 rotation_point = glm::vec2(), 
-               double angle = 0, SDL_RendererFlip flip = SDL_FLIP_NONE) :
-               src_rect(src_rect), rotation_point(rotation_point), angle(angle), flip(flip)
+               double angle = 0, SDL_RendererFlip flip = SDL_FLIP_NONE)
 {
     texture = load_texture(asset_path);
+    this->src_rect = src_rect;
+    this->rotation_point = rotation_point;
+    this->angle = angle;
+    this->flip = flip;
     SDL_QueryTexture(texture, NULL, NULL, &width, &height);
 }
