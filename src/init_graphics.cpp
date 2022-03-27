efd845f20caeebddff9dc12dbc1b34942d6753c3
alve9999 <alve.lindell@gmail.com>
2022-03-27 10:58:03 +0200

    moved graphic init

---
+++ src/init_graphics.cpp
@@ -0,0 +1,23 @@
+#include "init_graphics.h"
+
+SDL_Window* init_graphics(int width, int height){
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width, height,0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+    }
+    return window;
+}
---
+++ src/init_graphics.h
@@ -0,0 +1,4 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+SDL_Window* init_graphics(int width, int height);
