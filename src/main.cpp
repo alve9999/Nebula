e6e7455160ac7511019eb0b354d26e893b496bb8
alve9999 <alve.lindell@gmail.com>
2022-03-27 19:30:28 +0200

    fixed framerate

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,24 @@
 
 int main(int argc, char* argv[])
 {
+    const int fps = 60;
+    const int frameDelay = 1000/fps;
+    Uint32 framestart;
+    int frametime;
+
 	init("Medieval Peasant Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 	while (is_running)
 	{
+        framestart = SDL_GetTicks();
+
 		handle_events();
 		update();
 		render();
+        frametime = SDL_GetTicks() - framestart;
+
+        if (frameDelay>frametime){
+            SDL_Delay(frameDelay-frametime);
+        }
 	}
 	clean();
 	return 0;
