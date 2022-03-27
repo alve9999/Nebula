89fb4b065b97f25dee0eb10c92eb6f1df85c7439
alve9999 <alve.lindell@gmail.com>
2022-03-27 16:47:13 +0200

    fixed stuff

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,4 @@
 #include "game.h"
-
 #include <SDL2/SDL.h>
 #include "Input.h"
 #include "player.h"
@@ -8,13 +7,13 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 bool is_running = false;
 
-Player player;
+Player* player;
 
 void init(const char* title, int x, int y, int width, int height, int flags) {
     window = SDL_CreateWindow(title, x, y, width, height, flags);
     renderer = SDL_CreateRenderer(window, -1, 0);
     is_running = true;
-    player = Player();
+    player = new Player();
 }
 
 void handle_events() {
@@ -33,13 +32,13 @@ void handle_events() {
 }
 
 void update() {
-    player.update();
+    player->update();
 }
 
 void render() {
     SDL_SetRenderDrawColor(renderer, 123, 30, 0, 255);
     SDL_RenderClear(renderer);
-    player.render();
+    player->render();
     SDL_RenderPresent(renderer);
 }
 
