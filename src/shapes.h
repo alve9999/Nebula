05f0cb2ed82878daf203f150396ca9a46007fced
alve9999 <alve.lindell@gmail.com>
2022-03-27 17:32:36 +0200

    fixed shapes

--- src/shapes.h
+++ src/shapes.h
@@ -2,14 +2,12 @@
 
 struct Rect
 {
-    int width, height;
-    glm::vec2 pos;
-    Rect(int width, int height,glm::vec2 pos);
+    int width, height,x,y;
+    Rect(int width, int height,int x, int y);
 };
 
 struct Circle 
 {
-    int radius;
-    glm::vec2 pos;
-    Circle(int radius,glm::vec2 pos);
+    int radius,x,y;
+    Circle(int radius,int x, int y);
 };
