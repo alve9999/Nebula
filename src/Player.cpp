4be43f2621a0244b93d05e2119a4d9c101cc1964
wallespion <vilhelm.lindell123@gmail.com>
2022-03-27 19:57:45 +0200

    added diagonal movement

--- src/Player.cpp
+++ src/Player.cpp
@@ -10,19 +10,15 @@ Player::Player() {
 
 void Player::update() {
     if (Input::KeyDown(SDL_SCANCODE_LEFT)) {
-        velocity.y = 0;
         velocity.x = -speed;
     }
     else if (Input::KeyDown(SDL_SCANCODE_UP)) {
         velocity.y = -speed;
-        velocity.x = 0;
     }
     else if (Input::KeyDown(SDL_SCANCODE_DOWN)) {
         velocity.y = speed;
-        velocity.x = 0;
     }
     else if (Input::KeyDown(SDL_SCANCODE_RIGHT)) {
-        velocity.y = 0;
         velocity.x = speed;
     }
     update_position();
