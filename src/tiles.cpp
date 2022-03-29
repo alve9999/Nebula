ee521515099fabaeb46a0da8e97774afd2a3a85e
alve9999 <alve.lindell@gmail.com>
2022-03-29 21:16:52 +0200

    fixed incorrect path

--- src/tiles.cpp
+++ src/tiles.cpp
@@ -6,7 +6,7 @@ extern Tile Tiles[4] = {};
 void init_tiles()
 {
     Tiles[0] = (Tile(nullptr, (int)TileType::None));
-    Tiles[1] = (Tile(new Sprite("assets/tiles/dirt.bmp"), (int)TileType::Solid));
-    Tiles[2] = (Tile(new Sprite("assets/tiles/grass.bmp"), (int)TileType::Solid));
-    Tiles[3] = (Tile(new Sprite("assets/tiles/stone.bmp"), (int)TileType::Solid));
+    Tiles[1] = (Tile(new Sprite("../sprites/dirt.bmp"), (int)TileType::Solid));
+    Tiles[2] = (Tile(new Sprite("../sprites/grass.bmp"), (int)TileType::Solid));
+    Tiles[3] = (Tile(new Sprite("../sprites/stone.bmp"), (int)TileType::Solid));
 }
